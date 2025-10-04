bbc69fe93800d6948dfd4d6ef3208e6aa723bcaa
Moon Le <tuyen.qa.vietnam@gmail.com>
2025-10-04 20:54:28 +0700

    day-09: complete feed-basic to learn promises basics

--- Pack-1-JS-TS-Core/atoms/javascript/js-04-02-promises-basics/src/feed-basic.js
+++ Pack-1-JS-TS-Core/atoms/javascript/js-04-02-promises-basics/src/feed-basic.js
@@ -6,29 +6,40 @@ export function getUserFromCache(id) {
   // TODO:
   // - If id === 1 -> resolve { id:1, name:'Alice' }
   // - Else -> reject 'User not found'
+    if(id === 1) return Promise.resolve({id: 1, name: 'Alice'});
+    return Promise.reject('User not found');
 }
 
 // 2) Chain: user -> feed
 export function getFeedForUser(user) {
   // TODO:
   // - Return Promise that resolves to array of strings: [ 'Feed for <name> #1', 'Feed for <name> #2' ]
+    return getUserFromCache(user.id)
+        .then(user => Promise.resolve([`Feed for ${user.name} #1`, `Feed for ${user.name} #2`]));
 }
 
 // 3) Parallel: user + notifications
 export function getNotifications(userId) {
   // TODO:
   // - Return Promise that resolves to [ 'N1 for <userId>', 'N2 for <userId>' ]
+    return Promise.resolve([`N1 for ${userId}`, `N2 for ${userId}`]);
 }
 
 // 4) Race: cache vs network
 export function getUserFromNetworkSlow(id) {
   // TODO:
   // - Resolve after ~60ms with { id, name:'Net-' + id }
+    return new Promise(res => {
+        setTimeout(() => res({id, name:'Net-' + id}), 60)
+    })
 }
 
 export function timeout(ms) {
   // TODO:
   // - Reject after ms with 'Timeout!'
+    return new Promise((_, reject) => {
+        setTimeout(() => reject('Timeout!'), ms)
+    })
 }
 
 export function getUserFromCacheOrNetwork(id) {
@@ -36,4 +47,8 @@ export function getUserFromCacheOrNetwork(id) {
   // - Try cache first: return getUserFromCache(id)
   // - If cache rejects, return Promise.race([ getUserFromNetworkSlow(id), timeout(30) ])
   // - Hint: use .catch to switch path
+    return getUserFromCache(id).catch(e => Promise.all([
+        getUserFromNetworkSlow(id),
+        timeout(30)
+    ]))
 }
